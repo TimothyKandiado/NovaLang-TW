ffbaa9466bf485bc2407d6be12b0dd60a9dea85c
TimothyKandiado <kandiadot@gmail.com>
2023-11-08 09:19:35 +0200

    code cleanups

--- src/math_interpreter/scanner/object.rs
+++ src/math_interpreter/scanner/object.rs
@@ -6,26 +6,6 @@ pub enum Object {
 }
 
 impl Object {
-    pub fn is_none(&self) -> bool {
-        match self {
-            Object::None => true,
-            _ => false,
-        }
-    }
-
-    pub fn is_number(&self) -> bool {
-        match self {
-            Object::Number(_) => true,
-            _ => false,
-        }
-    }
-
-    pub fn is_string(&self) -> bool {
-        match self {
-            Object::String(_) => true,
-            _ => false,
-        }
-    }
 
     pub fn to_string(&self) -> String {
         match self {
